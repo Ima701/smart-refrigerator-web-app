18515a9f86df7dc9029bc5cc611547ea34fd5ebe
Ima701 <rhimashi707@gmail.com>
2026-07-03 00:35:05 +0530

    hotfix/debug notification function

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-970124e6'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.kn4ta6upbm"
+    "revision": "0.9riq2bg2jpo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
